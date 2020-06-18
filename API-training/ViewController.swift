0443da9b15e08312e0ef563a66a217028a02da9b
Nonoka Yamamoto <garden.n1234567890@gmail.com>
2020-06-19 01:03:03 +0900

    Initial Commit

---
+++ API-training/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  API-training
+//
+//  Created by 山本ののか on 2020/06/19.
+//  Copyright © 2020 Nonoka Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
